ce8b2b8b069cf1e03440c49e920d4b63ead6402c
Ihor Pankieiev <igorpankeyev@gmail.com>
2022-10-28 21:01:42 +0300

    Initial commit2

--- src/task4/Computer.java
+++ src/task4/Computer.java
@@ -3,14 +3,7 @@ package task4;
 
 public class Computer {
 
-    int[] computers = new int[5];
+    Computer[] Computers = new Computer[5];
 
-    void computer (){
-        for (int i = 0; i < computers.length; i++){
-            computers[i] = (int)((Math.random() * 30) - 10);
-            System.out.print(" " + computers[i]);
-        }
-        System.out.println();
-    }
 
 }
--- src/task4/One.java
+++ src/task4/One.java
@@ -2,16 +2,36 @@ package task4;
 
 public class One {
     public static void main(String[] args) {
-        Computer comp1 = new Computer();
-        Computer comp2 = new Computer();
-        Computer comp3 = new Computer();
-        Computer comp4 = new Computer();
-        Computer comp5 = new Computer();
-//        comp.computers = new int[10];
-        comp1.computer();
-        comp2.computer();
-        comp3.computer();
-        comp4.computer();
-        comp5.computer();
+        Computer obj1 = new Computer();
+        Computer obj2 = new Computer();
+        Computer obj3 = new Computer();
+        Computer obj4 = new Computer();
+        Computer obj5 = new Computer();
+
+        for (int i = 0; i < obj1.Computers.length; i++){
+            System.out.print(" " + obj1.Computers[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < obj2.Computers.length; i++){
+            System.out.print(" " + obj2.Computers[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < obj3.Computers.length; i++){
+            System.out.print(" " + obj3.Computers[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < obj4.Computers.length; i++){
+            System.out.print(" " + obj4.Computers[i]);
+        }
+        System.out.println();
+
+        for (int i = 0; i < obj5.Computers.length; i++){
+            System.out.print(" " + obj5.Computers[i]);
+        }
+        System.out.println();
+
     }
 }
